e2c0417525db006d34e09448704e839a54a61738
BCHBenoit Chabaneix <benoit.chabaneix@kellogg.northwestern.edu>
2024-05-14 17:38:16 +0000

    guardado7

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -7,7 +7,10 @@ def show
 end
 
 def new
-  @place_id = params["place_id"]
+  @places_table = Place.all
+  @places_row_id = places_table.find_by({"id" => params["place_id"]})
+  @place_id = places_row_id["id"]
+  @place_name = places_row_id["name"]
 end
 
 def create
