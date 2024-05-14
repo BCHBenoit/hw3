f6dbba470a085f7996ea09671a14c0657b30cb04
BCHBenoit Chabaneix <benoit.chabaneix@kellogg.northwestern.edu>
2024-05-14 14:37:40 +0000

    guardado3

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -10,6 +10,11 @@ class PlacesController < ApplicationController
   end
 
   def create
+    @new_row_places = Place.new
+    @places_name = params["name"]
+    @new_row_places["name"] = @places_name
+    @new_row_places.save
+    redirect_to "/places"
   end
 
 end
